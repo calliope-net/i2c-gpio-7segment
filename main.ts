f3e2aa98b2905b5add6f8f6216ba86eae8a860d1
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-08-17 16:05:42 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    leseKeypad()
+})
+function leseKeypad () {
+    z = qwiickeypad.getButton(qwiickeypad.eADDR.KEY_Qwiic)
+    if (bit.between(z, 48, 57)) {
+        qwiicgpio.writeOUTPUT_PORT(qwiicgpio.eADDR.GPIO_Qwiic, qwiicgpio.not(qwiicgpio.siebenSegment(z - 48, false)))
+    } else {
+        qwiicgpio.writeOUTPUT_PORT(qwiicgpio.eADDR.GPIO_Qwiic, qwiicgpio.not(qwiicgpio.siebenSegment(64, true)))
+    }
+}
+pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
+    basic.turnRgbLedOff()
+})
+pins.onPulsed(DigitalPin.P1, PulseValue.High, function () {
+    basic.setLedColor(0x0000ff)
+    leseKeypad()
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showNumber(qwiickeypad.getButton(qwiickeypad.eADDR.KEY_Qwiic))
+})
+let z = 0
+qwiicgpio.setMode(
+qwiicgpio.eADDR.GPIO_Qwiic,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT
+)
