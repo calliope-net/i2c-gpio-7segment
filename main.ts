c323581f3188dfb243b9dc96a2b0aa3d519027e8
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-10-06 11:20:10 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 function leseKeypad () {
     z = qwiickeypad.getButton(qwiickeypad.qwiickeypad_eADDR(qwiickeypad.eADDR.Keypad_x4B))
     if (bit.between(z, 48, 57)) {
-        qwiicgpio.writeOUTPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x26), qwiicgpio.siebenSegment(z - 48, false))
+        qwiicgpio.writeOUTPUT_PORT(i2cGPIO, qwiicgpio.siebenSegment(z - 48, false))
     } else {
-        qwiicgpio.writeOUTPUT_PORT(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x26), 192)
+        qwiicgpio.writeOUTPUT_PORT(i2cGPIO, 192)
     }
 }
 pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
@@ -20,8 +20,10 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     basic.showNumber(qwiickeypad.getButton(qwiickeypad.qwiickeypad_eADDR(qwiickeypad.eADDR.Keypad_x4B)))
 })
 let z = 0
+let i2cGPIO = 0
+i2cGPIO = qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27)
 qwiicgpio.setMode(
-qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x26),
+i2cGPIO,
 qwiicgpio.eIO.OUT,
 qwiicgpio.eIO.OUT,
 qwiicgpio.eIO.OUT,
